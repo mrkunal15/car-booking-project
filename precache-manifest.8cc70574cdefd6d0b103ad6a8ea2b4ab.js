68e4395ed5bda9aab4f98f1614b7be4f149128b5
mekunal15 <kunallaheri115@gmail.com>
2020-05-02 19:22:10 +0530

    Updates

---
+++ precache-manifest.8cc70574cdefd6d0b103ad6a8ea2b4ab.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd71ea849a93ab6d76c3fc9a7ee1e953",
+    "url": "/car-booking-project/index.html"
+  },
+  {
+    "revision": "6bab3735a03e94e0a9a3",
+    "url": "/car-booking-project/static/css/main.67417d5c.chunk.css"
+  },
+  {
+    "revision": "18477bcbd761ba802db4",
+    "url": "/car-booking-project/static/js/2.b60aed71.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/car-booking-project/static/js/2.b60aed71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bab3735a03e94e0a9a3",
+    "url": "/car-booking-project/static/js/main.4e638552.chunk.js"
+  },
+  {
+    "revision": "a146ba032ff2743c9981",
+    "url": "/car-booking-project/static/js/runtime-main.22438003.js"
+  },
+  {
+    "revision": "ac5564e560990ac8dd1df210c296f2fb",
+    "url": "/car-booking-project/static/media/car.ac5564e5.png"
+  },
+  {
+    "revision": "b7843a220a7301275f65a324f999dfc8",
+    "url": "/car-booking-project/static/media/pic.b7843a22.jpg"
+  }
+]);
